c553cd4bdc78365de1cf9e1f2baa8db3f28b8782
Oleg Sitnikov <o.sitnikov@pr-solution.ru>
2019-01-21 22:49:26 +0300

    update lib

--- app/src/main/java/com/marcosholgado/droidcon/home/features/top_stories/TopStoriesFragment.kt
+++ app/src/main/java/com/marcosholgado/droidcon/home/features/top_stories/TopStoriesFragment.kt
@@ -2,12 +2,12 @@ package com.marcosholgado.droidcon.home.features.top_stories
 
 import androidx.navigation.fragment.findNavController
 import com.marcosholgado.articlelist.ArticleListFragment
-import com.marcosholgado.droidcon.home.features.top_stories.TopStoriesFragmentDirections.action_articles_to_reader
+import com.marcosholgado.droidcon.home.features.top_stories.TopStoriesFragmentDirections.*
 
 class TopStoriesFragment: ArticleListFragment() {
 
     override fun onItemSelected(url: String) {
-        val navDirections = action_articles_to_reader(url)
+        val navDirections = actionArticlesToReader(url)
         findNavController().navigate(navDirections)
     }
 }
